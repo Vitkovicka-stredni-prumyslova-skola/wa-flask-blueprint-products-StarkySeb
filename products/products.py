151187ad5774a21362c0d4617d7c44554e044d46
Sebastián Starý <sebastian.stary@student.vitkovickastredni.cz>
2024-03-19 13:41:50 +0100

    CV - nový produkt v API

--- products/products.py
+++ products/products.py
@@ -40,6 +40,70 @@ def detailOfProduct(id):
 
 @products_bp.route('/products/add', methods=['GET','POST'])
 def uploadProduct():
+    #podminka pro post, pokud nejsou odeslane data z formulare, nacte se webova stranka
+    if request.method == 'POST':
+        #nacteni produktu z formulare v new-product.html
+        product_name = request.form.get('productName')
+        product_description = request.form.get('productDescription')
+        product_price = request.form.get('productPrice')
+        product_category = request.form.get('productCategory')
+        #podminka pro vyplneni vsech udaju, pokud neni splnena podminka vyhodi response
+        if not all([product_name, product_description, product_price, product_category]):
+            return "Please fill in all the form fields."
+        #pouziti nahodneho generovani cisel v danem rozmezi pro pocet kusu produktu a pro hodnoceni produktu
+        random_int = random.randint(100, 1000)
+        random_rate = random.uniform(0.0, 5.0)
+        #nacteni obrazu ze static souboru
+        image = "static/img/noimage.png"
+        #nacitani z formulare pro ulozeni dat do databaze fakeapistore
+        product_data = {
+            'title': product_name,
+            'price': float(product_price),
+            'description': product_description,
+            'category': product_category,
+            'image': image,
+            'rating': {'rate':random_rate,'count':random_int}
+        }
+        #posilani dat do fakeapistore databaze
+        fakestore_api_url = "https://fakestoreapi.com/products"
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(fakestore_api_url, data=json.dumps(product_data), headers=headers)
+        #podminka pro spravne nacteni produktu do databaze
+        if response.status_code == 201:
+            return "Upload successful"
+        else:
+            return "Upload failed."
+    #nacteni databaze
+    MyData = GetAllProducts()
+    #nacteni nazvu kategorie z promenne data do promenne categories
+    categories = set(product["category"] for product in MyData)
+    sortedCategory = sorted(categories)
+    categories_count = {}
+    for product in MyData:
+        category = product["category"]
+        categories_count[category] = categories_count.get(category, 0) + 1
+    return render_template('products/new-product.html', products=MyData, categories=sortedCategory, pocetProduktu=categories_count)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+@products_bp.route('/products/add', methods=['GET','POST'])
+def uploadProduct():
      if request.method == 'POST':
         # Zpracování formuláře
         product_name = request.form.get('productName')
@@ -59,14 +123,15 @@ def uploadProduct():
         l = len(productId)
         random_int = random.randint(100, 1000)
         random_rate = random.uniform(0.0, 5.0)
-        
+        image = "static/img/noimage.png"
+
         product_data = {
             'id': l + 1,
             'title': product_name,
             'price': float(product_price),
             'description': product_description,
             'category': product_category,
-            'image': '',
+            'image': image,
             'rating': {'rate':random_rate,'count':random_int}  
         }
         headers = {'Content-Type': 'application/json'}
@@ -80,12 +145,13 @@ def uploadProduct():
             # Handle the case where the upload failed
             return "Upload failed."
 
-     data = GetAllProducts()
-     l = len(data)
-     categories = set(product["category"] for product in data)
+     MyData = GetAllProducts()
+     l = len(MyData)
+     categories = set(product["category"] for product in MyData)
      sortedCategory = sorted(categories)
      categories_count = {}
-     for product in data:
+     for product in MyData:
         category = product["category"]
         categories_count[category] = categories_count.get(category, 0) + 1
-     return render_template('products/new-product.html', products = data, categories=sortedCategory, pocetProduktu = categories_count)
+     return render_template('products/new-product.html', products = MyData, categories=sortedCategory, pocetProduktu = categories_count)
+"""
